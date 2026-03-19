ba595046330843fe978c3a07971db77f0edc4a00
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-03-19 04:00:53 +0000

    updates

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="row text-center">
+				<h1 className="text-center mt-5">Contador</h1>
+				<div className="col">
+					<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="col counter">
+					{props.digThree}
+				</div>
+				<div className="col counter">
+					{props.digTwo}
+				</div>
+				<div className="col counter">
+					{props.digOne}
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,21 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
+let contador=0
+
+setInterval(() => {
+
+let One = Math.floor((contador/1)%100)
+let Two = Math.floor((contador/10)%100)
+let Three = Math.floor((contador/100)%100)
+
+contador++
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home digThree={Three} digOne={One} digTwo={Two}/>  
   </React.StrictMode>,
 )
+}, 1000)
+
+
